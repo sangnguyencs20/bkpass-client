af51344c5a47991ed910ee6c759750680046aa45
svn <81409431+sangnguyencs20@users.noreply.github.com>
2023-01-02 20:33:43 +0700

    test

--- src/api/APIRoutes.js
+++ src/api/APIRoutes.js
@@ -1,5 +1,5 @@
-export const host = "https://bkpassserver.up.railway.app"
-export const client = "https://bkpassclient.up.railway.app"
+export const host = "https://bkpass-server.up.railway.app"
+export const client = "https://bkpass-client.up.railway.app"
 export const registerRoute = `${host}/api/register`
 export const loginRoute = `${host}/api/login`
 export const addItemRoute = `${host}/api/add-item`
